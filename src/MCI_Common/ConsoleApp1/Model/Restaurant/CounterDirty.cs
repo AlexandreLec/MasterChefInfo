262a36936bfb4a64434a88aec57aa61d2549ca4c
Martinhubz <martinhub92@gmail.com>
2018-12-06 11:24:21 +0100

    dirty counter

---
+++ src/MCI_Common/ConsoleApp1/Model/Restaurant/CounterDirty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Room.Model.Restaurant
+{
+    class CounterDirty 
+    {
+        /// <summary>
+        /// Number of elements on the dirty counter
+        /// </summary>
+        private int nbPlates { get; set; }
+        private int nbGlasses { get; set; }
+        private int nbSilverware { get; set; }
+
+
+
+    }
+}
