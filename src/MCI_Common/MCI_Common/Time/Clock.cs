5d47444f1d54b41095f89ca74c1fb6eba851e518
Martinhubz <martinhub92@gmail.com>
2018-12-08 14:47:48 +0100

    Time added, sets period for whole app

---
+++ src/MCI_Common/MCI_Common/Time/Clock.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCI_Common.Time
+{
+
+    public sealed class Clock
+    {
+        /// <summary>
+        /// Singleton variables (thread-safe method)
+        /// </summary>
+        private static Clock instance = null;
+        private static readonly object padlock = new object();
+
+
+        /// <summary>
+        /// Period of time (in milliseconds), 1sec default, can be set
+        /// </summary>
+        private int _period = 60000 ;
+        public int Period
+        {
+            get { return this._period / _speed; }
+            set { this._period = value; }
+        }
+
+        /// <summary>
+        /// Time is divided by speed 
+        /// (ex: if period = 1min and speed = 2x, final period = 30sec (1000ms/2 = 500ms) )
+        /// </summary>
+        private int _speed = 1;
+        public int Speed
+        {
+            get { return this._speed; }
+            set { this._speed = value; }
+        }
+
+
+        /// <summary>
+        /// Singleton implementation
+        /// </summary>
+        public static Clock Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Clock();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+    }
+}
+
--- src/MCI_Common/Room/Main/Main.cs
+++ src/MCI_Common/Room/Main/Main.cs
@@ -1,9 +1,11 @@
 ﻿using MCI_Common.Recipes;
+using MCI_Common.Time;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Room.Main
@@ -12,7 +14,16 @@ namespace Room.Main
     {
         static void Main(string[] args)
         {
-           
+            Clock.Instance.Speed = 2;
+            Clock.Instance.Period = 6000;
+
+            Console.WriteLine("Waiting {0} seconds (speed = 2)", Clock.Instance.Period);
+            Thread.Sleep(Clock.Instance.Period);
+
+            Clock.Instance.Speed = 1;
+            Console.WriteLine("Waiting {0} seconds (speed = 1)", Clock.Instance.Period);
+            Thread.Sleep(Clock.Instance.Period);
+
         }
     }
 }
